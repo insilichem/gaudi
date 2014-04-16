2f8f306f3ca2cd2a8613ee45d17fe6aa680f8c6a
Jaime RG <jaime.rogue@gmail.com>
2014-04-16 12:58:42 +0200

    Bye FETRA. Hi, MOF3D

--- fetra/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-# FETRA (F3T2A)
-# Force-field free target-driven docking algorithm
-# A docking module for UCSF Chimera
-# Jaime RGP <https://bitbucket.org/jrgp> @ UAB, 2014
-
-import build, score, utils
