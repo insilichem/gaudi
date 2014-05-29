2db62b7ab805bf31db61a077675282662fac2d46
Jaime RG <jaime.rogue@gmail.com>
2014-05-29 18:21:55 +0200

    Deeper defaults

--- alum_run.py
+++ alum_run.py
@@ -5,5 +5,5 @@ pdbs = glob.glob(sys.argv[1]+"*.pdb")
 for pdb in pdbs:
 	mol = chimera.openModels.open(pdb)
 	chimera.runCommand('del element.H')
-	chimera.runCommand('runscript /home/jr/x/hyde/alum.py -p 200 -g 10 -t 2.0')
+	chimera.runCommand('runscript /home/jr/x/hyde/alum.py -p 300 -g 15 -t 2.0')
 	chimera.openModels.close(mol)
