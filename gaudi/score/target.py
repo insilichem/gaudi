60ab725737f93c54694ce446e8d27e40bfdafd11
Jaime RG <jaime.rogue@gmail.com>
2014-06-09 13:03:00 +0200

    New default arguments; distances and dihedrals are treated individually, not averaged.

--- gaudi/score/target.py
+++ gaudi/score/target.py
@@ -4,20 +4,20 @@
 # Genetic Algorithm for Unified Docking Inference
 # A docking module for UCSF Chimera
 # Jaime RGP <https://bitbucket.org/jrgp> @ UAB, 2014
-import numpy, chimera
-def distance(atoms, target, threshold, wall=True):
+import chimera
+def distance(atoms, target, threshold, threshold2=0.0):
 	distances = []
 	for a in atoms:
 		d = _distance(a, target)
 		if threshold == 'covalent':
 			threshold = chimera.Element.bondLength(a.element, target.element)
 		
-		if d < threshold and wall:
+		if d < threshold2:
 			distances.append(1000)
 		else:
 			distances.append(d-threshold)
 	
-	return numpy.mean(distances)
+	return distances
 
 ## Internal use
 def _distance(atom1, atom2):
