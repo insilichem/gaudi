e95f9d8757d3ad8857521a9dd66aebc4ec5cc51a
Jaime RG <jaime.rogue@gmail.com>
2015-05-29 14:26:44 +0200

    refactor(objective.geometry): merged simplecoordination and coordination rmsd in single objective, callable with a different <method> keyword

--- gaudi/objectives/coordination.py
+++ gaudi/objectives/coordination.py
@@ -20,7 +20,7 @@ Good planarity is assured by a dihedral check.
 
 :probe:     The atom that acts as the metal center
 :radius:    Distance from `probe` where ligating atoms must be found
-:atomtypes: Types of atoms that are considered ligands to `probe`
+:atom_types: Types of atoms that are considered ligands to `probe`
 :residues:  Type of residues that must coordinate to `probe`
 :distance:  Target distance from `probe` to ligating atoms
 :angle:     Target angle `probe`, ligand and neighbor should form ideally
@@ -32,6 +32,7 @@ import logging
 import numpy
 # Chimera
 import chimera
+import MetalGeom
 # GAUDI
 from gaudi.objectives import ObjectiveProvider
 import gaudi.parse
@@ -45,40 +46,103 @@ def enable(**kwargs):
 
 class SimpleCoordination(ObjectiveProvider):
 
-    def __init__(self, probe=None, radius=None, atomtypes=None, residues=None,
-                 distance=None, angle=None, dihedral=None, enforce_all_residues=False,
-                 *args, **kwargs):
+    def __init__(self, method='simple', probe=None, radius=None, atom_types=None, residues=None,
+                 distance=None, angle=None, dihedral=None, min_atoms=1,
+                 enforce_all_residues=False, only_one_ligand_per_residue=False, *args, **kwargs):
         ObjectiveProvider.__init__(self, **kwargs)
+        self.method = method
         self.probe = self._getatom(probe)
         self.residues = set(self._getresidue(*residues))
         self.radius = radius
-        self.atomtypes = atomtypes
+        self.atom_types = atom_types
         self.distance = distance
         self.angle = angle
         self.dihedral = dihedral
+        self.min_atoms = min_atoms
+        self.only_one_ligand_per_residue = only_one_ligand_per_residue
         self.enforce_all_residues = enforce_all_residues
         self.molecules = tuple(g.compound.mol for g in self.parent.genes.values()
                                if g.__class__.__name__ == "Molecule")
 
-    def evaluate(self):
+        if self.method == 'metalgeom':
+            self.evaluate = self.evaluate_MetalGeom
+        else:
+            self.evaluate = self.evaluate_simple
+
+    def evaluate_simple(self):
+        """
+        1. For every atom within the search radius that matches the criteria, get:
+            - Coordinates for `self.probe`, atom, its immediate neighbor, and next neighbor
+              not being terminal
+            - Angle formed by probe, atom, and neighbor
+            - Dihedral formed by probe, atom, neighbor, and next neighbor
+        2. The score returned is the sum of averages of:
+            - Absolute difference between `self.distance` and found atoms. That way,
+              atoms that are within requested distance, get scores near zero.
+            - Absolute difference of sines of `self.angle` and formed angles. Good matches
+              tend to zero.
+            - Absolute sine of dihedrals. If they are coplanar, this should be zero.
+        3. As a result, lower scores are better.
+        """
+        try:
+            atoms_by_distance = self._get_nearest_atoms()
+        except (NotEnoughAtomsError, SomeResiduesMissingError):
+            return -1000 * self.weight
+        else:
+            distances, angles, dihedrals = [], [], []
+            for d, a2 in atoms_by_distance:
+                # Get coords for needed atoms
+                distances.append(d)
+                if self.dihedral or self.angle:
+                    c1 = self._get_xform_coord(self.probe)  # the probe
+                    c2 = self._get_xform_coord(a2)  # the ligand is a2
+                    a3 = a2.neighbors[0]  # bonded atom to ligand
+                    c3 = self._get_xform_coord(a3)
+                    a4 = next(a for a in a3.neighbors  # neighbor of a3 that is not ligand
+                              if a is not a3 and len(a.neighbors) > 1)
+                    c4 = self._get_xform_coord(a4)
+
+                    if self.angle:
+                        angle = chimera.angle(c1, c2, c3)
+                        delta = abs(math.cos(math.radians(self.angle))
+                                    - math.cos(math.radians(angle)))
+                        angles.append(delta)
+
+                    if self.dihedral:
+                        dihedral = chimera.dihedral(c1, c2, c3, c4)
+                        planarity = abs(math.sin(math.radians(dihedral)))
+                        dihedrals.append(planarity)
+
+            return sum(numpy.average(x) for x in (distances, angles, dihedrals) if x)
+
+    def evaluate_MetalGeom(self):
+        """
+        1. Get requested atoms sorted by distance
+        2. If they meet the minimum quantity, return the rmsd for
+           that geometry
+        3. If that's not possible of they are not enough, return penalty
+        """
+        try:
+            test_atoms = [a for d, a in self._get_nearest_atoms()]
+        except (NotEnoughAtomsError, SomeResiduesMissingError):
+            return -1000 * self.weight
+        else:
+            try:
+                rmsd, center, vectors = MetalGeom.gui.geomDistEval(
+                    self.geometry, self.target, test_atoms)
+            except:  #
+                logger.warning(
+                    "Geometry not feasible in current conditions")
+                rmsd = -1000 * self.weight
+            return rmsd
+
+    def _get_nearest_atoms(self):
         """
         1. Get atoms and residues found within `self.radius` angstroms from `self.probe`
         1.1. Found residues MUST include self.residues. Otherwise, apply penalty
         2. Sort atoms by absolute difference of `self.distance` and distance to `self.probe`.
            That way, nearest atoms are computed first.
         2.1. If found atoms do not include some of the requested types, apply penalty.
-        3. For every atom that matches our criteria, calculate
-            - Coordinates for `self.probe`, atom, its immediate neighbor, and next neighbor
-              not being terminal
-            - Angle formed by probe, atom, and neighbor
-            - Dihedral formed by probe, atom, neighbor, and next neighbor
-        4. The score returned is the sum of averages of:
-            - Absolute difference between `self.distance` and found atoms. That way,
-              atoms that are within requested distance, get scores near zero.
-            - Absolute difference of sines of `self.angle` and formed angles. Good matches
-              tend to zero.
-            - Absolute sine of dihedrals. If they are coplanar, this should be zero.
-        5. As a result, lower scores are better.
         """
         self._update_env()
         atoms = self.env.atoms()
@@ -87,48 +151,19 @@ class SimpleCoordination(ObjectiveProvider):
         # (distance, ligand) tuple, sorted by distances
         atoms_by_distance = \
             [(abs(self.distance - self.probe.xformCoord().distance(a.xformCoord())),
-              a) for a in atoms if a.name in self.atomtypes and a.residue in self.residues]
+              a) for a in atoms if a.name in self.atom_types and a.residue in self.residues]
         found_residues = set(a.residue for d, a in atoms_by_distance)
-        if not atoms_by_distance:
+        if len(atoms_by_distance) < self.min_atoms:
             logger.warning(
                 "Could not find requested atoms from residues in probe environment")
-            return self.weight * -1000
+            raise NotEnoughAtomsError
         if self.enforce_all_residues and found_residues != self.residues:
             logger.warning(
                 "Some atoms found, but some residues are missing")
-            return self.weight * -1000
+            raise SomeResiduesMissingError
 
         atoms_by_distance.sort()
-        # Prevent several atoms per residue.
-        # Only nearest is added, since distances is sorted
-        # residues = set()
-        distances, angles, dihedrals = [], [], []
-        for d, a2 in atoms_by_distance:
-            # if a2.residue not in residues:
-                # Get coords for needed atoms
-            distances.append(d)
-            if self.dihedral or self.angle:
-                c1 = self._get_xform_coord(self.probe)  # the probe
-                c2 = self._get_xform_coord(a2)  # the ligand is a2
-                a3 = a2.neighbors[0]  # bonded atom to ligand
-                c3 = self._get_xform_coord(a3)
-                a4 = next(a for a in a3.neighbors  # neighbor of a3 that is not ligand
-                          if a is not a3 and len(a.neighbors) > 1)
-                c4 = self._get_xform_coord(a4)
-
-                if self.angle:
-                    angle = chimera.angle(c1, c2, c3)
-                    delta = abs(math.cos(math.radians(self.angle))
-                                - math.cos(math.radians(angle)))
-                    angles.append(delta)
-
-                if self.dihedral:
-                    dihedral = chimera.dihedral(c1, c2, c3, c4)
-                    planarity = abs(math.sin(math.radians(dihedral)))
-                    dihedrals.append(planarity)
-
-                # residues.add(a2.residue)
-        return sum(numpy.average(x) for x in (distances, angles, dihedrals) if x)
+        return atoms_by_distance
 
     # TODO: Probes get lost if rotamers are applied!
     def _getatom(self, probe):
@@ -192,3 +227,11 @@ class SimpleCoordination(ObjectiveProvider):
         Return transformed coordinates from atom `a`
         """
         return a.molecule.openState.xform.apply(a.coord())
+
+
+class NotEnoughAtomsError(Exception):
+    pass
+
+
+class SomeResiduesMissingError(Exception):
+    pass
