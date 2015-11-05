83efd67963c7c977cbd6cf418b6bc4582a8bc6f5
Jaime RG <jaime.rogue@gmail.com>
2015-11-05 19:07:38 +0100

    Reworked benchmark and RMSD scripts

--- scripts/benchmarker.py
+++ scripts/benchmarker.py
@@ -13,6 +13,7 @@
 """
 Sequential runs of a given GAUDI essay in all folders contained under
 given path. Useful for benchmarks.
+
 """
 
 from __future__ import print_function
@@ -21,7 +22,6 @@ import os
 import subprocess
 import sys
 import tempfile
-
 import yaml
 
 
@@ -31,55 +31,73 @@ def arguments():
     parser = argparse.ArgumentParser(
         description='Benchmarks a set given a template.')
     parser.add_argument('template', metavar='/path/to/template.gaudi-input', type=str,
-                        help='A GAUDI input file that will serve as template.'
-                        'Protein and ligand genes should be named as "Protein" '
-                        'and "Ligand", respectively.')
+                        help='A GAUDI input file that will serve as template.')
     parser.add_argument('dataset', metavar='/path/to/dataset', type=str,
                         help='Location of the benchmark dataset.')
     parser.add_argument('--protein', metavar='protein_name', type=str,
-                        default='protein.mol2', help='Tagname of the protein file')
+                        default='protein.mol2', help='Filename of the protein file')
     parser.add_argument('--ligand', metavar='ligand_name', type=str,
-                        default='ligand.mol2', help='Tagname of the ligand file')
+                        default='ligand.mol2', help='Filename of the ligand file. '
+                        'It is supposed to be the real solution, as well.')
+    parser.add_argument('--results', metavar='folder_name', type=str,
+                        default='results', help='Name of the output folder.')
     parser.add_argument('--chimera', metavar='/path/to/bin/chimera', type=str,
                         default='~/.local/UCSF-Chimera64-1.10.1/bin/chimera',
-                        help='Path to UCSF Chimera binaries')
+                        help='Path to UCSF Chimera binary')
     parser.add_argument('--gaudi', metavar='/path/to/gaudi/launch.py', type=str,
                         default=gaudi_launch)
 
     return parser.parse_args()
 
 
-def main():
-    args = arguments()
+def run(args):
+    """
+    Load a common template with at least two ``molecule`` instances:
+    one called ``Protein`` and a second called ``Ligand``. Then, it replaces
+    the original paths in the template with those found in the subsequent 
+    directories and launches a new Chimera instance to perform the essay.
+    """
 
+    # Load input template
     with open(args.template, 'r') as f:
         template = yaml.load(f)
 
+    # Find indices of Protein and Ligand genes
     prot_index = next(
         i for (i, g) in enumerate(template['genes']) if g['name'] == 'Protein')
     ligand_index = next(
         i for (i, g) in enumerate(template['genes']) if g['name'] == 'Ligand')
 
+    # Scan directories in supplied dataset
     dirs = os.listdir(args.dataset)
     for i, d in enumerate(dirs):
-        print("Processing item", d)
+        # 1 - Perform calculation
+        print("Processing item ", i+1, ": ", d)
+        # Replace original output path with a subfolder in current dataset
         template['general']['outputpath'] = os.path.join(args.dataset, d,
-                                                         'results')
+                                                         args.results)
 
-        template['genes'][0]['path'] = os.path.join(
+        # Replace original Ligand and Protein paths with new ones
+        template['genes'][prot_index]['path'] = os.path.join(
             args.dataset, d, args.protein)
-        template['genes'][1]['path'] = os.path.join(
+        template['genes'][ligand_index]['path'] = os.path.join(
             args.dataset, d, args.ligand)
 
+        # Write the overwritten template to a temp location
         number, filename = tempfile.mkstemp()
         with open(filename, 'w') as f:
             f.write(yaml.dump(template))
 
+        # Launch the essay!
         command = [os.path.expanduser(args.chimera), '--nogui',
                    '--silent', '--script', '{} {}'.format(args.gaudi, filename)]
         subprocess.call(command)
-        print(
-            "Processed {}/{} ({}%)".format(i, len(dirs), 100 * i / len(dirs)))
+
+        # Report progress after job is completed
+        print("Processed {}/{} ({}%)".format(
+            i+1, len(dirs), 100 * (i+1) / len(dirs)))
+
 
 if __name__ == "__main__":
-    main()
+    args = arguments()
+    run(args)
--- scripts/rmsd.py
+++ scripts/rmsd.py
@@ -14,13 +14,17 @@
 This script calculates RMSD between two molecule poses in batch mode.
 It should be provided with the path to the folders and the name of the
 reference ligand.
+
+It requires rdkit for fast RMSD calculations.
 """
 
-import argparse
-import csv
-import os
 from rdkit import Chem
 from rdkit.Chem import rdMolAlign
+import argparse
+import csv
+import itertools
+import numpy
+import os
 import yaml
 import zipfile
 
@@ -41,6 +45,14 @@ def parse_args():
 
 
 def ligands_from_zip(d, args):
+    """
+    Open all the listed solutions in *.gaudi-output file and create
+    a generator that yields the name and mol2 data of the ligand of
+    each solution.
+
+    As a temporary workaround, the ligand is considered
+    to be the lightest file with a mol2 extension.
+    """
     with open(os.path.join(args.dataset, d, args.results, args.file)) as f:
         data = yaml.load(f)
         for filename in data['GAUDI.results']:
@@ -68,9 +80,20 @@ def calculate_rmsd(ligand, reference):
     return -3.0
 
 
-def main():
-    args = parse_args()
-    with open('rmsd2.csv', 'w+') as csvfile:
+def run(args):
+    """
+    For each folder in dataset, open its results and:
+        Extract ligand.mol2 from zipfile
+        Open reference.mol2
+        Calculate RMSD
+    Print resulting RMSD values to CSV file
+    If some error occurred, the RMSD value will be set to a negative float:
+        -1.0: The reference molecule could not be loaded
+        -2.0: The ligand molecule could not be loaded
+        -3.0: Ref and ligand were loaded, but RMSD calculation failed
+    """
+
+    with open(os.path.join(args.dataset, 'rmsd.csv'), 'w+') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=['test', 'ligand', 'rmsd'])
         writer.writeheader()
         for d in os.listdir(args.dataset):
@@ -97,5 +120,45 @@ def main():
                         writer.writerow(
                             {'test': d, 'ligand': name, 'rmsd': rmsd})
 
+
+def stats(great_threshold=1.5, good_threshold=2.5):
+    """ Run through the CSV file and get some stats """
+    with open(os.path.join(args.dataset, 'rmsd.csv')) as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        filtered_csv = filter(lambda x: float(x[2]) > 0, reader)
+        grouped_csv = itertools.groupby(filtered_csv, lambda x: x[0])
+
+    with open(os.path.join(args.dataset, 'rmsd.txt')) as txt:
+        total, greats, goods, accs, avg_rmsd_first = 0, [], [], [], []
+        for k, g in grouped_csv:
+            group = list(g)
+            pdb, result, rmsd = min(group, key=lambda x: x[2])
+            rmsd = float(rmsd)
+            avg_rmsd_first.append(next(
+                float(c) for (a, b, c) in group if b.endswith('_000_Ligand.mol2')))
+            total += 1
+            if rmsd < great_threshold:
+                txt.write('{}\t{}\t{}\tGREAT\n'.format(pdb, result, rmsd))
+                if result == 'dock_000_Ligand.mol2':
+                    accs.append(rmsd)
+                greats.append(rmsd)
+                goods.append(rmsd)
+            elif rmsd < good_threshold:
+                txt.write('{}\t{}\t{}\GOOD\n'.format(pdb, result, rmsd))
+                goods.append(rmsd)
+            else:
+                txt.write('{}\t{}\t{}\n'.format(pdb, result, rmsd))
+
+        txt.write('Poses < 1.5: {}%\n\tAvg RMSD: {}A\n'.format(
+                  100 * len(greats) / total, numpy.mean(greats)))
+        txt.write('Poses < 2.5: {}%\n\tAvg RMSD: {}A\n'.format(
+                  100 * len(goods) / total, numpy.mean(goods)))
+        txt.write('Accurate hits: {}%\n\tAvg RMSD: {}A\n'.format(
+                  100 * len(accs) / total, numpy.mean(accs)))
+        txt.write('Avg RMSD of first result: {}A\n'.format(
+                  numpy.mean(avg_rmsd_first)))
+
 if __name__ == '__main__':
-    main()
+    args = parse_args()
+    run(args)
+    stats(args)
