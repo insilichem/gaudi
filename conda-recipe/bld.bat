611439791c895573fb0bdfa1e0cb89b05efe4877
Jaime RGP <jaime.rogue@gmail.com>
2017-11-07 17:14:21 +0100

    conda-recipe: pin prody version  to 1.8.2

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -1,3 +1,3 @@
-pip install pychimera munch voluptuous click boltons deap pyyaml prody
+pip install pychimera munch voluptuous click boltons deap pyyaml prody==1.8.2
 if errorlevel 1 exit 1
 python setup.py install
--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pip install pychimera munch voluptuous click boltons deap pyyaml prody
+pip install pychimera munch voluptuous click boltons deap pyyaml prody==1.8.2
 $PYTHON setup.py install
