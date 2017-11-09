5317bdae6009b81a4a75c5233d51d5cde3c5735e
Jaime RGP <jaime.rogue@gmail.com>
2017-11-09 18:55:27 +0100

    tests: return exit code

--- tests/run_test.py
+++ tests/run_test.py
@@ -4,17 +4,17 @@
 ##############
 # GaudiMM: Genetic Algorithms with Unrestricted
 # Descriptors for Intuitive Molecular Modeling
-# 
+#
 # https://github.com/insilichem/gaudi
 #
 # Copyright 2017 Jaime Rodriguez-Guerra, Jean-Didier Marechal
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,4 +29,4 @@ from pychimera import patch_environ, enable_chimera
 if __name__ == '__main__':
     patch_environ()
     enable_chimera()
-    pytest.main(sys.argv[1:])
+    sys.exit(pytest.main(sys.argv[1:]))
