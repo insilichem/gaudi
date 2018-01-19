cae5da1b846491cf068781d94aaea5979504b62d
Jaime RGP <jaime.rogue@gmail.com>
2018-01-19 12:07:40 +0100

    fix(tests): test_genes_trajectory typo in function name

--- tests/test_genes_trajectory.py
+++ tests/test_genes_trajectory.py
@@ -4,17 +4,17 @@
 ##############
 # GaudiMM: Genetic Algorithms with Unrestricted
 # Descriptors for Intuitive Molecular Modeling
-# 
+#
 # https://github.com/insilichem/gaudi
 #
 # Copyright 2017 Jaime Rodriguez-Guerra, Jean-Didier Marechal
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -60,8 +60,8 @@ def test_trajectory(individual, mol_path, trajectory_path, frame, max_frame):
 @pytest.mark.parametrize("mol_path, trajectory_path, frame, max_frame", [
     ('1amb.pdb', '1amb.dcd', 0, 1),
 ])
-def test_benchmark_rotamers(benchmark, individual, mol_path, trajectory_path,
-                            frame, max_frame):
+def test_benchmark_trajectory(benchmark, individual, mol_path, trajectory_path,
+                              frame, max_frame):
     @benchmark
     def run():
         trajectory_setup(individual, mol_path, trajectory_path, frame, max_frame)
