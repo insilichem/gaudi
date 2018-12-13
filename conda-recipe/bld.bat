76e430d6b4be977f62bb7807e2f0e9bac2bbd49a
Jaime RGP <jaime.rogue@gmail.com>
2018-12-13 19:07:38 +0100

    Attempt to fix travis builds

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -1,3 +1,3 @@
-pip install pychimera munch voluptuous click boltons deap pyyaml prody==1.8.2
+%PYTHON% -m pip install pychimera munch voluptuous click boltons deap pyyaml prody==1.8.2 -vv
 if errorlevel 1 exit 1
-python setup.py install
+%PYTHON% -m pip install . --no-deps -vv
--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pip install pychimera munch voluptuous click boltons deap pyyaml prody==1.8.2
-$PYTHON setup.py install
+$PYTHON -m pip install pychimera munch voluptuous click boltons deap pyyaml prody==1.8.2 -vv
+$PYTHON -m pip install . --no-deps -vv
