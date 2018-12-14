5c4453ea6defa63241fbbc79b2357565363bdaae
Jaime RGP <jaime.rogue@gmail.com>
2018-12-14 15:38:48 +0100

    fix(CI,recipe): move pypi packages to conda if possible

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -1,3 +1,3 @@
-%PYTHON% -m pip install pychimera munch voluptuous click boltons deap pyyaml prody==1.8.2 -vv
+%PYTHON% -m pip install pychimera prody==1.8.2 -vv
 if errorlevel 1 exit 1
 %PYTHON% -m pip install . --no-deps -vv
--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-$PYTHON -m pip install pychimera munch voluptuous click boltons deap pyyaml prody==1.8.2 -vv
+$PYTHON -m pip install pychimera prody==1.8.2 -vv
 $PYTHON -m pip install . --no-deps -vv
