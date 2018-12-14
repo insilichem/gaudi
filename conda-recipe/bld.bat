a93a2c86a7786eae7bdbda124a9385a896d19f62
Jaime RGP <jaime.rogue@gmail.com>
2018-12-14 17:34:40 +0100

    fix(CI): try to avoid unicode errors with conda build under py3k

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -1,3 +1,3 @@
-%PYTHON% -m pip install pychimera prody==1.8.2 -vv
+%PYTHON% -m pip install pychimera prody==1.8.2 -v
 if errorlevel 1 exit 1
-%PYTHON% -m pip install . --no-deps -vv
+%PYTHON% -m pip install . --no-deps -v
--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-$PYTHON -m pip install pychimera prody==1.8.2 -vv
-$PYTHON setup.py install
+$PYTHON -m pip install pychimera prody==1.8.2 -v
+$PYTHON -m pip install . --no-deps -v
