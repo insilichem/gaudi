1c3319fcb5d0c23be46c9fc6e07bdcbb42ee888a
Jaime RGP <jaime.rogue@gmail.com>
2019-01-24 17:37:02 +0100

    Modernize recipe

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -1,3 +1,3 @@
-%PYTHON% -m pip install pychimera prody==1.8.2 -v
+%PYTHON% -m pip install prody==1.8.2
 if errorlevel 1 exit 1
 %PYTHON% -m pip install . --no-deps -v
--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-$PYTHON -m pip install pychimera prody==1.8.2 -v
+$PYTHON -m pip install prody==1.8.2
 $PYTHON -m pip install . --no-deps -v
