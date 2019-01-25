4f676142ea9d06457a91e8a13a3aded2d8d8ae10
Jaime RGP <jaime.rogue@gmail.com>
2019-01-25 20:05:50 +0100

    ci: use conda package built for prody (no more pip dependencies!)

--- conda-recipe/bld.bat
+++
@@ -1,3 +0,0 @@
-%PYTHON% -m pip install prody==1.8.2
-if errorlevel 1 exit 1
-%PYTHON% -m pip install . --no-deps -v
--- conda-recipe/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-$PYTHON -m pip install prody==1.8.2
-$PYTHON -m pip install . --no-deps -v
