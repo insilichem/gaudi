46cca68572ded3943f16ac47cc3a0d6ea5a78871
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2019-02-13 17:49:54 +0100

    Modernize recipe [WIP] (#11)
    * Modernize recipe
    
    * ci: update travis script to build pychimera out of PR#13
    
    * merge master
    
    * ci: get ucsf chimera installation script from pychimera's repo
    
    * ci: use conda package built for prody (no more pip dependencies!)
    
    * ci: prody conda
    
    * ci: fix local channel
    
    * fix ci
    
    * ci: fix local packages channel url
    
    * ci: update conda recipe location for pychimera
    
    * ci: use pychimera 0.2.7
    
    * ci: don't cache miniconda
    
    * ci: update numpy in OSX chimera
    
    * ci: remove problematic packages in Chimera installation for OSX
    
    * ci: update OSX Chimera's dateutil (pandas req)
    
    * ci: latest pandas requires numpy >= 1.12
    
    * conda-recipe: include drugscorex package for osx
    
    * conda-recipe: drugscorex is not working properly on osx; skip for now

--- conda-recipe/bld.bat
+++
@@ -1,3 +0,0 @@
-%PYTHON% -m pip install pychimera prody==1.8.2 -v
-if errorlevel 1 exit 1
-%PYTHON% -m pip install . --no-deps -v
--- conda-recipe/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-$PYTHON -m pip install pychimera prody==1.8.2 -v
-$PYTHON -m pip install . --no-deps -v
